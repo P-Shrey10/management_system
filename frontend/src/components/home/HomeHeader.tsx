87485879619dced7fc0c44470cce556af98030ba
p-shrey10 <pradhanangashrey@gmail.com>
2025-03-22 21:55:36 +0545

    signup button is added in home page

--- frontend/src/components/home/HomeHeader.tsx
+++ frontend/src/components/home/HomeHeader.tsx
@@ -9,13 +9,18 @@ const HomeHeader = () => {
       <div>
         <img src={Logo} alt="Logo Image" className="h-14" />
       </div>
-      <div>
+      <div className="flex space-x-4">
         <button
           onClick={() => navigate("/login")}
-          className="px-4 py-2 text-white rounded-lg hover:bg-blue-700"
-          style={{ backgroundColor: "#614F7F" }}
+          className="px-4 py-2 text-white rounded-lg bg-[#614F7F] hover:bg-[#523F6B]"
         >
           Login
+        </button>
+        <button
+          onClick={() => navigate("/signup")}
+          className="px-4 py-2 text-white rounded-lg bg-[#614F7F] hover:bg-[#523F6B]"
+        >
+          Sign Up
         </button>
       </div>
     </div>
