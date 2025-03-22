14d7f39aec16ef4db849a72a655c5208ea9ece5e
p-shrey10 <pradhanangashrey@gmail.com>
2025-03-22 21:55:12 +0545

    toastify is added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <App />
     </BrowserRouter>
   </React.StrictMode>
